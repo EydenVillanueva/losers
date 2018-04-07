f6977650d7ebfaeed7e3eb55543795a8a7b068d6
EydenVillanueva <eydenv85@gmail.com>
2018-04-06 22:34:58 -0500

    Creación de modelo Publicación

--- publicaciones/models.py
+++ publicaciones/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Publicacion(models.Model):
+    nombre = models.CharField(max_length=300)
+    contenido = models.TextField()
+    imagen_fondo = models.FileField()
+    fecha = models.DateField()
+
+    def __str__(self):
+        return self.nombre
+        
